aebf8eb5560b1a726aeb8e0850fdd560c1d56152
Giant <giantninja908@protonmail.com>
2021-06-27 17:38:59 -0400

    fix settings import not working

--- src/preload.js
+++ src/preload.js
@@ -21,3 +21,28 @@ let scripts =
     const runScripts = () => { scripts.forEach(script => {(script)()}); }
 
 if (config.allowScripts) runScripts()
+
+window.prompt = () => { // import settings fix
+  var tempHTML = '<div class="setHed">Import Settings</div>';
+  tempHTML +=
+      '<div class="settName" id="importSettings_div" style="display:block">Settings String<input type="url" placeholder="Paste Settings String Here" name="url" class="inputGrey2" id="settingString"></div>';
+  tempHTML += '<a class="+" id="importBtn">Import</a>';
+  menuWindow.innerHTML = tempHTML;
+  importBtn.addEventListener('click',
+                             () => { parseSettings(settingString.value); });
+
+  function parseSettings(string) {
+    if (string && string != '') {
+      try {
+        var json = JSON.parse(string);
+        for (var setting in json) {
+          setSetting(setting, json[setting]);
+          showWindow(1);
+        }
+      } catch (err) {
+        console.error(err);
+        alert('Error importing settings.');
+      }
+    }
+  }
+};
